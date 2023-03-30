8481b9cfb99da02aa246df98f28224ba27b354d5
RezaAzharii <rezaazhari867@gmail.com>
2023-03-30 11:35:13 +0700

    membuat prosedural quicksort

--- Quicksort_011.cpp
+++ Quicksort_011.cpp
@@ -37,3 +37,54 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
+void q_sort(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)
+		return;														// langkah 1
+
+	//Partition the list into two parts:
+	// one containing elements less that or equal to pivot
+	//Outher containing elements greather than pivot 
+
+	pivot = arr[low];											    //Langkah 2
+
+    i = low + 1;													//Langkah 3
+	j = high;														//Langkah 4
+
+	while (i <= j)
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))					//Langkah 5
+		{
+			i++;													//Langkah 6				
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or rqual to pivot
+		while ((arr[j] > pivot) && (j >= low))						//Langkah 7
+		{
+			j--;
+			cmp_count++;											//Langkah 8
+		}
+		cmp_count++;
+																	//Langkah 9
+		if (i < j) //if the greater element is on the left of the element
+		{
+			//swap the element at index i whit the element at index
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted list
+	if (low < j)													//Langkah 11
+	{
+		//move the pivot to its correct potition in the list
+		swap(low, j);
+		mov_count++;
+	}
+	q_sort(low, j - i);												//Langkah 12
+
+	//sort the list on the right of pivot using quick sort
+	q_sort(j + i, high);											//Langkah 13
+}
